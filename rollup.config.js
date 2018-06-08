e375d288f98be8d5f0f59cb7fc250cb341adf361
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-06-08 19:42:16 +0300

    Updated deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
-import uglify from 'rollup-plugin-uglify'
+import {uglify} from 'rollup-plugin-uglify'
 
 const build = ({NODE_ENV, format, suffix}) => ({
   input: 'src/infestines.js',
